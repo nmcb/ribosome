36d2b0f4d2be7e0539e279fa6621bb68972453c6
nmcb <nmcb@xs4all.nl>
2025-07-15 16:36:52 +0200

    added rna matching

--- src/test/scala/emc/rna/InterpolationSpecification.scala
+++ src/test/scala/emc/rna/InterpolationSpecification.scala
@@ -10,6 +10,7 @@ class InterpolationSpecification extends AnyFunSpec with Matchers:
   val rna        = RNA(C, C, C)
   val codon      = Codon(C, C, C)
   val nucleotide = C
+  val seq        = Seq(C, C, C)
 
   describe("String interpolation"):
 
@@ -24,3 +25,14 @@ class InterpolationSpecification extends AnyFunSpec with Matchers:
 
     it("injects valid nucleotides"):
       rna"AUGC$nucleotide" should be(RNA(A, U, G, C, C))
+
+    it("injects valid nucleotide sequences"):
+      rna"AUGC$seq" should be(RNA(A, U, G, C, C, C, C))
+
+  
+  describe("RNA matching"):
+    
+    it("matches valid rna sequences"):
+      val rna"AUG${actual1}AUG${actual2}" = rna"AUGCCCAUGAAA": @unchecked
+      actual1 should be(RNA(C, C, C))
+      actual2 should be(RNA(A, A, A))
